3fdc0f383c7059c181d1240e8cb5f4f29c86f2cf
Julia <julia.zhou4@student.tdsb.on.ca>
2022-04-26 18:47:39 -0400

    actually added dialogue

--- dialogue/Dialogue.java
+++ dialogue/Dialogue.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Dialogue {
 
     public static void main(String[] args){
-        System.out.println("ass");
+        String[] dialogue = {"never", "gonna", "give","you","up"};
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        char keepGoing = 'c';
+        
+        while(keepGoing == 'c' && i < 5){
+            System.out.println(dialogue[i]);
+            keepGoing = sc.next().charAt(0);
+            i++;
+        }
+
     }
 
 }
