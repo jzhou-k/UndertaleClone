3da16b52971b59488811c302be90734db52ba8e8
Julia <julia.zhou4@student.tdsb.on.ca>
2022-04-28 17:15:44 -0400

    added combatobject and setting

--- CombatObject.java
+++ CombatObject.java
@@ -1,3 +1,43 @@
 public class CombatObject {
-    int coord = 1;
+    
+    //this object will take info from settings 
+    //default speed will be taken from settings 
+
+    String name; 
+    int speed;
+    int[] coord = new int[2]; //(x,y)
+    int[] size = new int[2]; //(Width,length), need to check collision
+
+    //default class contructor
+    public CombatObject(String name){
+        this.name = name;
+        //default coordinate is (0,0) (theres gotta be a better way of doing this)
+        this.coord[0] = 0;
+        this.coord[1] = 0;
+        //default size is (1,1)
+        this.size[0] = 1;
+        this.size[0] = 1;
+        this.speed = 10;
+
+    }
+
+    public CombatObject(String name, int[] coord, int[] size){
+        this.name = name;
+        this.coord = coord; 
+        this.size = size;
+        this.speed = 10;
+    }
+
+
+    //what does public header do again? 
+    //what does static do? 
+    public void move(int x, int y) {
+        //coord of this particular object is changed
+        coord[0] = x;
+        coord[1] = y;
+    }
+
+    public int[] currentCoord(){
+        return this.coord;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("This is where we run our main game and also do testing");
+        CombatObject player = new CombatObject("patrick");
 
+        player.move(12, 2);
+        int[] playerCoord = player.currentCoord();
+        System.out.println(playerCoord[0] + ","  + playerCoord[1]);
 
     }
 }
