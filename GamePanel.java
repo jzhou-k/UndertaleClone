e8e303ab2c653647c4bd6029f926d5367445210d
Julia <julia.zhou4@student.tdsb.on.ca>
2022-05-02 16:02:21 -0400

    added game thread and implemented runnable (I have no idea what they do)

--- GamePanel.java
+++ GamePanel.java
@@ -3,7 +3,7 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-public class GamePanel  extends JPanel {
+public class GamePanel  extends JPanel implements Runnable{
     
     //game panel is a subclass of Jpanel 
     //SCREEN SETTINGS
@@ -12,11 +12,21 @@ public class GamePanel  extends JPanel {
     final int screenWidth = 16 * 20 ;
     final int screenHeight = 16 * 20;
 
+    //what is gameThread its something to do with time and fps? 
+    Thread gameThread;
+
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true); //make game run better 
+    }
+
+    //what happens when you implement something? 
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
     }
 
 
