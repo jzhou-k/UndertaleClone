faac1072b3ec431a1d5287b3ce9f5886a9911ee3
PatrickZitang <99358683+PatrickZitang@users.noreply.github.com>
2022-05-06 14:15:00 -0400

    asd
    qwasd

--- GameObject.java
+++ GameObject.java
@@ -1,3 +1,7 @@
+/**
+ *
+ * @author sunzi
+ */
 public class GameObject {
     String name; 
     int[] coord = new int[2]; //(x,y)
@@ -7,5 +11,40 @@ public class GameObject {
         this.name = name;
         this.coord = coord; 
         this.size = size;
+    }
+    
+    /*
+    Accessors
+    */
+
+    public String getName()
+    {
+        return this.name; 
+        
+        //method overiding for npc to be stationary.
+    }
+    
+    public int[] getCoord()
+    {
+        return this.coord;
+    }
+            
+    public int[] getSize()
+    {
+        return this.size;
+    }
+    //mutators
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public void setCoord (int[] coord){
+        this.coord = coord;
+    }
+    
+    public void setSize (int[] size){
+        this.size = size; 
     }
 }
