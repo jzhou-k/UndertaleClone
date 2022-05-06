a9380be67d49c9b9528920b3e7b818c5b7fc6318
Julia <julia.zhou4@student.tdsb.on.ca>
2022-05-06 12:47:09 -0400

    updated gameobject, added item

--- GameObject.java
+++ GameObject.java
@@ -3,4 +3,9 @@ public class GameObject {
     int[] coord = new int[2]; //(x,y)
     int[] size = new int[2];
 
+    public GameObject(String name, int[] coord, int[] size){
+        this.name = name;
+        this.coord = coord; 
+        this.size = size;
+    }
 }
